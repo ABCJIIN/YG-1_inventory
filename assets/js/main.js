1db9ac61dde805038f1ef5619d1c3fa9e8522e29
ABCJIIN <97517088+ABCJIIN@users.noreply.github.com>
2023-06-26 17:57:07 +0900

    0626/17:57/jinny

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,19 @@
-// 로그인 페이지 login.asp index.html
-    function frmSUB()
-    {
-        if(frm.id.value=="")
-        {
-            alert("Please Input your ID.");
-            frm.id.focus();
-            return false;
-        }
-        if(frm.pwd.value=="")
-        {
-            alert("Please Input your Password.");
-            frm.pwd.focus();
-            return false;
-        }
-        frm.submit();
-    }
-    
+// 로그인 페이지를 제외한 공통 헤더 푸터 INCLUDE
+$(document).ready(function(){
+    $("#subContainer #header").load("../../html/include/header.html");//헤더 INCLUDE
+    $("#subContainer #footer").load("../../html/include/footer.html");//푸터 INCLUDE
+});
+
+//Multi search 버튼
+$(document).ready(function() {
+    $('.multi').click(function(){
+        $('.multi-contents').toggle();
+    });
+});
+
+//Multi search 내부 tag-close 버튼 삭제 기능
+$(document).ready(function() {
+    $('.tag-close').click(function(){
+        $(this).parent('div').hide();
+    });
+});
